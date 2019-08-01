67fd670e94aca7389f7e85eb499bad8b484c8376
JordanCottle <jordancottle622@gmail.com>
2019-08-01 19:34:16 -0400

    Fixed menu
    Polished responsiveness and animation speeds

--- Main.c
+++ Main.c
@@ -42,10 +42,13 @@ menu_states menuSelect = TOP;
 #define HUNGER_BAR_Y 104
 #define HAPPINESS_BAR_Y 118
 #define STATUS_BAR_X 8
+#define OFFSET 10
+#define MENU_BULLET_WIDTH 16
+#define MENU_BULLET_HEIGHT 16
 
 uint8_t hunger = 0;
 uint8_t happiness = 100;
-
+uint8_t menuIndex = 0;
 
 uint32_t readADC(uint32_t, uint32_t);
 
@@ -87,19 +90,18 @@ void tickHandler(){
             if(happiness < 5){
                 happiness = 0;
             }else{
-                happiness-=5;
+                happiness--;;
             }
 
     }
 
     if(happiness > 100){
         happiness = 100;
-    }else
-    if(hunger > MAX_HUNGER){
+    }else if(hunger > MAX_HUNGER){
         hunger = MAX_HUNGER;
     }
 
-    if(hunger == MAX_HUNGER){
+    if(hunger >= MAX_HUNGER){
         petState = DEAD;
     }else if(happiness == 0){
         petState = LOST;
@@ -107,7 +109,7 @@ void tickHandler(){
 }
 
 void imageHandler() {
-    TimerIntClear(TIMER_BASE, TIMER_TIMA_TIMEOUT);
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
     switch(imageFrame) {
         case BASE: 
             imageFrame = BONUS;
@@ -117,14 +119,86 @@ void imageHandler() {
             break;
     }
 
+    switch(gameState){
+         case IDLE:
+            idleState();
+            break;
+         case FEEDING:
+            feedingState();
+            break;
+         case WALKING:
+            walkingState();
+            break;
+         case MENU:
+            menuState();
+            break;
+        }
 
+    drawStatusHunger();
+    drawStatusHappiness();
+}
+
+void menuHandler(){
+    if(gameState != MENU){
+        return;
+    }
+
+    TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
+    uint32_t verticalAxis = readADC(ADC1, ADC_SEQUENCER);
+
+    if(verticalAxis > 3000){
+        // clear previous bullet from screen
+        ST7735_FillRect(32, 26, MENU_BULLET_WIDTH, MENU_BULLET_HEIGHT * 5, ST7735_Color565(0, 0, 0));
+        if(menuIndex == 2){
+            menuIndex = 0;
+        }else{
+            menuIndex++;
+        }
+    }else if(verticalAxis < 1000){
+        // clear previous bullet from screen
+        ST7735_FillRect(32, 26, MENU_BULLET_WIDTH, MENU_BULLET_HEIGHT * 5, ST7735_Color565(0, 0, 0));
+        if(menuIndex == 0){
+            menuIndex = 2;
+        }else{
+            menuIndex--;
+        }
+    }
+
+    switch(menuIndex){
+        case 0:
+            menuSelect = BOTTOM;
+            break;
+        case 1:
+            menuSelect = MIDDLE;
+            break;
+        case 2:
+            menuSelect = TOP;
+            break;
+    }
 }
 
 
 void stickInterruptHandler(){
     SysCtlDelay(DEBOUNCE_DELAY);
     if(!GPIOPinRead(STICK_CLICK_PORT, STICK_CLICK_PIN)){
-        gameState = MENU;
+        if(gameState == MENU){
+            switch(menuSelect){
+                case TOP:
+                    gameState = WALKING;
+                    break;
+                case MIDDLE:
+                    gameState = FEEDING;
+                    break;
+                case BOTTOM:
+                    gameState = IDLE;
+                    break;
+                default:
+                    return;
+            }
+        }else{
+            gameState = MENU;
+        }
+
         clearScreen();
     }
     GPIOIntClear(STICK_CLICK_PORT, 0xFF); // clear interrupt flag
@@ -160,6 +234,8 @@ void init(){
 
     // initialize tick counter
     initPeriodicTimer(TIMER_PERIPH, TIMER_BASE, TICK_DELAY, tickHandler);
+    initPeriodicTimer(SYSCTL_PERIPH_TIMER1, TIMER1_BASE, TICK_DELAY/2, imageHandler);
+    initPeriodicTimer(SYSCTL_PERIPH_TIMER2, TIMER2_BASE, TICK_DELAY/2, menuHandler);
 }
 
 uint32_t readADC(uint32_t adcBase, uint32_t sequencer){
@@ -181,8 +257,9 @@ void drawSprite(uint16_t image[], uint16_t x, uint16_t y){
 void drawStatusHunger(){
     if(hunger <= 100){
         ST7735_FillRect(STATUS_BAR_X, HUNGER_BAR_Y, hunger, 12, ST7735_Color565(6, 86, 117));
+        ST7735_FillRect(STATUS_BAR_X + hunger, HUNGER_BAR_Y, 100-hunger, 12, ST7735_Color565(30, 30, 30));
     }else{
-        ST7735_FillRect(STATUS_BAR_X, HUNGER_BAR_Y, MAX_HUNGER, 12, ST7735_Color565(0, 0, 255));
+        ST7735_FillRect(STATUS_BAR_X, HUNGER_BAR_Y, 100, 12, ST7735_Color565(0, 0, 255));
     }
 }
 
@@ -212,10 +289,29 @@ void feedingState () {
 }
 
 void walkingState() {
+    static uint8_t idleCount = 0;
     uint32_t verticalAxis = readADC(ADC0, ADC_ACCEL_SEQUENCER);
+
+    // if acceleration is in dead zone, count up
+    if(verticalAxis < 2500 && verticalAxis > 1500){
+        idleCount++;
+    }else{
+        idleCount = 0;
+    }
+
+
     switch(petState) {
         case ALIVE:
-            drawSprite(ROCK_IDLE, SPRITE_X-verticalAxis , SPRITE_Y);
+
+            if(imageFrame == BASE){
+                // clear last sprite drawn
+                ST7735_FillRect(SPRITE_X-16, SPRITE_Y-IMAGE_HEIGHT+1, 32, IMAGE_HEIGHT, ST7735_Color565(0, 0, 0));
+                drawSprite(ROCK_IDLE, SPRITE_X + 16 , SPRITE_Y);
+            }else{
+                ST7735_FillRect(SPRITE_X+IMAGE_WIDTH-16, SPRITE_Y-IMAGE_HEIGHT+1, 32, IMAGE_HEIGHT, ST7735_Color565(0, 0, 0));
+                drawSprite(ROCK_IDLE, SPRITE_X - 16 , SPRITE_Y);
+            }
+
             break;
         case DEAD:
         case LOST:
@@ -225,6 +321,13 @@ void walkingState() {
           gameState = IDLE;
           petState = ALIVE;
     }
+
+    // if the idle count gets too high, return to idle state
+    if(idleCount > 15){
+        ST7735_FillRect(0, 0, 128, 100, ST7735_Color565(0, 0, 0));
+        idleCount = 0;
+        gameState = IDLE;
+    }
 }
 
 void idleState(){
@@ -233,7 +336,7 @@ void idleState(){
             drawSprite(ROCK_IDLE, SPRITE_X, SPRITE_Y);
             break;
         case LOST:
-            // draw nothing
+            // clear sprite area
             ST7735_FillRect(0, 0, 128, 100, ST7735_Color565(0, 0, 0));
             break;
         case DEAD:
@@ -247,26 +350,20 @@ void idleState(){
 }
 
 void menuState(){
-    //static char *menu = "MENU";
-    const uint8_t OFFSET = 16;
-    
     ST7735_DrawString(10, 1, "MENU", ST7735_Color565(255, 255, 255));
     ST7735_DrawString(10, 3, "Walk!", ST7735_Color565(255, 255, 255));
     ST7735_DrawString(10, 5, "Eat!", ST7735_Color565(255, 255, 255));
-    ST7735_DrawString(10, 7, "Exit!", ST7735_Color565(255, 255, 255));
+    ST7735_DrawString(10, 7, "Idle!", ST7735_Color565(255, 255, 255));
 
     switch(menuSelect){
     case TOP:
-        ST7735_FillRect(32, 26, 90, 16, ST7735_Color565(80, 80, 80));
-        ST7735_DrawString(10, 3, "Walk!", ST7735_Color565(255, 255, 255));
+        ST7735_FillRect(32, 26, MENU_BULLET_WIDTH, MENU_BULLET_HEIGHT, ST7735_Color565(80, 80, 80));
         break;
     case MIDDLE:
-        ST7735_FillRect(32, 26 + OFFSET*2, 90, 16, ST7735_Color565(120, 120, 120));
-        ST7735_DrawString(10, 5, "Eat!", ST7735_Color565(255, 255, 255));
+        ST7735_FillRect(32, 26 + OFFSET*2, MENU_BULLET_WIDTH, MENU_BULLET_HEIGHT, ST7735_Color565(120, 120, 120));
         break;
     case BOTTOM:
-        ST7735_FillRect(32, 26 + OFFSET*4, 90, 16, ST7735_Color565(160, 160, 160));
-        ST7735_DrawString(10, 7, "Exit!", ST7735_Color565(255, 255, 255));
+        ST7735_FillRect(32, 26 + OFFSET*4, MENU_BULLET_WIDTH, MENU_BULLET_HEIGHT, ST7735_Color565(160, 160, 160));
         break;
     default:
         return;
@@ -280,25 +377,6 @@ int main(void)
     clearScreen();
 
     while(1){
-        switch(gameState){
-            case IDLE:
-                idleState();
-                break;
-            case MENU:
-                menuState();
-                break;
-            case FEEDING:
-                feedingState();
-                break;
-            case WALKING:
-                walkingState();
-                break;
-            default:
-                // bad state
-                gameState = IDLE;
-        }
 
-        drawStatusHunger();
-        drawStatusHappiness();
     }
 }
