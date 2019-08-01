feedf5f909354d56a865d04912321fd9c8291832
JordanCottle <jordancottle622@gmail.com>
2019-08-01 17:08:00 -0400

    Centered Sprite
    Status bars

--- Main.c
+++ Main.c
@@ -24,17 +24,27 @@
 
 #include "images.h"
 
-typedef enum {ALIVE, LOST, DEAD} pet_states;
+
 typedef enum {MENU, IDLE, FEEDING, WALKING} game_states;
+typedef enum {ALIVE, LOST, DEAD} pet_states;
 typedef enum {BASE, BONUS} image_states;
 
-#define SPRITE_X 32
-#define SPRITE_Y 32
+game_states gameState = IDLE;
+pet_states petState = ALIVE;
+image_states imageFrame = BASE;
 
-void updateFrequency();
-void toggleSound();
-void updateInputDelay();
-void setPWMPeriod(uint32_t);
+#define SPRITE_X 40
+#define SPRITE_Y 80
+#define TICK_DELAY 32000000 // 1/4 second
+#define MAX_HUNGER 100
+#define HUNGER_BAR_Y 104
+#define HAPPINESS_BAR_Y 118
+#define STATUS_BAR_X 8
+
+uint8_t hunger = 0;
+uint8_t happiness = 100;
+
+
 uint32_t readADC(uint32_t, uint32_t);
 
 // Cool sketch function to test joystick input
@@ -53,9 +63,33 @@ void clearScreen(){
     ST7735_FillScreen(ST7735_Color565(0, 0, 0));
 }
 
-void timerInterruptHandler(){
+void tickHandler(){
     TimerIntClear(TIMER_BASE, TIMER_TIMA_TIMEOUT);
-    sketch();
+    //sketch();
+
+    switch(gameState){
+        case FEEDING:
+            hunger -= 5;
+            happiness--;
+            break;
+        case WALKING:
+            hunger += 3;
+            happiness += 5;
+            break;
+        default:
+            hunger++;
+            happiness--;
+    }
+
+    if(happiness > 100){
+        happiness = 100;
+    }
+
+    if(hunger > MAX_HUNGER + 20){
+        petState = DEAD;
+    }else if(happiness == 0){
+        petState = LOST;
+    }
 }
 
 
@@ -75,7 +109,7 @@ void stickInterruptHandler(){
 //    GPIOIntClear(BUTTON_PORT, 0xFF); // clear interrupt flag
 //}
 
-#define TIMER_DELAY 16000000/32 // 1/8 second
+
 
 void init(){
     // configure clock
@@ -95,7 +129,8 @@ void init(){
     initADC(ADC1_PERIPH, ADC1, ADC_SEQUENCER, VERTICAL_ADC_CHANNEL,
             VERTICAL_AXIS_PERIPH, VERTICAL_AXIS_PORT, VERTICAL_AXIS_PIN);
 
-    initPeriodicTimer(TIMER_PERIPH, TIMER_BASE, TIMER_DELAY, timerInterruptHandler);
+    // initialize tick counter
+    initPeriodicTimer(TIMER_PERIPH, TIMER_BASE, TICK_DELAY, tickHandler);
 }
 
 uint32_t readADC(uint32_t adcBase, uint32_t sequencer){
@@ -113,12 +148,20 @@ void drawSprite(uint16_t image[], uint16_t x, uint16_t y){
     ST7735_DrawBitmap(x, y, image, 68, 48);
 }
 
-uint32_t happiness = 100;
-uint32_t hunger = 0;
 
-game_states gameState = IDLE;
-pet_states petState = ALIVE;
-image_states imageFrame = BASE;
+void drawStatusHunger(){
+    if(hunger <= MAX_HUNGER){
+        ST7735_FillRect(STATUS_BAR_X, HUNGER_BAR_Y, hunger, 12, ST7735_Color565(6, 86, 117));
+    }else{
+        ST7735_FillRect(STATUS_BAR_X, HUNGER_BAR_Y, MAX_HUNGER, 12, ST7735_Color565(0, 0, 255));
+    }
+}
+
+void drawStatusHappiness(){
+    ST7735_FillRect(STATUS_BAR_X, HAPPINESS_BAR_Y, happiness, 12, ST7735_Color565(0, 255, 0));
+    ST7735_FillRect(STATUS_BAR_X + happiness, HAPPINESS_BAR_Y, 100-happiness, 12, ST7735_Color565(0, 0, 255));
+}
+
 
 void feedingState () {
     switch(petState) {
@@ -155,9 +198,20 @@ void walkingState() {
 void idleState(){
     switch(petState){
         case ALIVE:
+            drawSprite(ROCK_IDLE, SPRITE_X, SPRITE_Y);
+            break;
+        case LOST:
+            // draw nothing
+            clearScreen();
+            break;
+        case DEAD:
+            drawSprite(ROCK_DEAD, SPRITE_X, SPRITE_Y);
+            break;
+        default: // bad state
+            petState = ALIVE;
+            gameState = IDLE;
             break;
     }
-
 }
 
 void menuState(){
@@ -188,5 +242,8 @@ int main(void)
                 // bad state
                 gameState = IDLE;
         }
+
+        drawStatusHunger();
+        drawStatusHappiness();
     }
 }
